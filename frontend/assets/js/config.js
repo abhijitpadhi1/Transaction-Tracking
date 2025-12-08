23854056f441308ef246323ec215c3499d6e8b4a
abhijitpadhi1 <abhijitpadhi1@gmail.com>
2025-12-08 15:30:23 +0530

    Deploy3

---
+++ frontend/assets/js/config.js
@@ -0,0 +1,12 @@
+// Detect environment automatically
+const isLocalhost =
+  window.location.hostname === "127.0.0.1" ||
+  window.location.hostname === "localhost";
+
+// IMPORTANT: Replace this with your actual HF space URL
+const HF_BASE_URL = "https://abhijitpadhi1-transaction-tracking.hf.space/";
+
+// Final BASE_URL which all other JS files should use
+window.API_BASE_URL = isLocal ? "http://127.0.0.1:8000" : HF_BASE_URL;
+
+// console.log("API Base URL:", BASE_URL);
